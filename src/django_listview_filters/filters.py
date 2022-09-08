0b8bfb24164b8485cdc0afa652d63da3e24bd044
Karl Wooster <karl@woostertech.com>
2022-09-07 22:59:30 -0700

    first attempt at 'relatedonly' clases

--- src/django_listview_filters/filters.py
+++ src/django_listview_filters/filters.py
@@ -382,3 +382,21 @@ class AllValuesFieldListFilter(FieldListViewFilter):
 
 
 FieldListViewFilter.register(lambda f: True, AllValuesFieldListFilter)
+
+
+class RelatedOnlyFieldListViewFilter(RelatedFieldListViewFilter):
+    def field_choices(self, field: models.Field, request):
+        return super().field_choices(field, request)
+
+
+class RelatedOnlyFieldListFilter(RelatedFieldListViewFilter):
+    def field_choices(self, field, request, model_admin):
+        pk_qs = (
+            model_admin.get_queryset(request)
+            .distinct()
+            .values_list("%s__pk" % self.field_path, flat=True)
+        )
+        ordering = self.field_admin_ordering(field, request, model_admin)
+        return field.get_choices(
+            include_blank=False, limit_choices_to={"pk__in": pk_qs}, ordering=ordering
+        )
